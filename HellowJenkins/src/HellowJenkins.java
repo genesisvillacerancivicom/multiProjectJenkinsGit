763210bdc4a55ebe0297cec0739c6550b979766f
genesis <genesis.villaceran@civi.com>
2018-08-20 17:21:51 -0400

    Test Codes for testing

---
+++ HellowJenkins/src/HellowJenkins.java
@@ -0,0 +1,24 @@
+
+public class HellowJenkins {
+
+	public static void main(String[] args) {
+		
+
+		System.out.println("############################");
+		System.out.println("############################");
+		System.out.println("############################");
+		System.out.println("############################");
+		System.out.println("############################");
+		System.out.println("############################");
+		System.out.println("Hellow Jenkins!!!!!!!!!!!");
+		System.out.println("############################");
+		System.out.println("############################");
+		System.out.println("############################");
+		System.out.println("############################");
+		System.out.println("############################");
+		System.out.println("############################");
+
+
+	}
+
+}
---
+++ HellowWorld/src/HelloWorld.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+
+/**
+ * @author gvillaceran
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("============================");
+		System.out.println("============================");
+		System.out.println("============================");
+		System.out.println("============================");
+		System.out.println("============================");
+		System.out.println("============================");
+		System.out.println("Hellow Worlds!!!!!!!!!!!");
+		System.out.println("============================");
+		System.out.println("============================");
+		System.out.println("============================");
+		System.out.println("============================");
+		System.out.println("============================");
+		System.out.println("============================");
+		System.out.println("============================");
+
+	}
+
+}
